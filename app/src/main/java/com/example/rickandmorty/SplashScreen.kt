01899bd755ce0c22cac748f7e3327d6737e0c960
Maxim Karassev <maximus_1901@mail.ru>
2023-10-08 19:02:35 +0600

    1 st commit Splash screen

---
+++ app/src/main/java/com/example/rickandmorty/SplashScreen.kt
@@ -0,0 +1,31 @@
+package com.example.rickandmorty
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper())
+            .postDelayed(
+                {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                    finish()
+                },
+                3000,
+            )
+
+
+    }
+}
